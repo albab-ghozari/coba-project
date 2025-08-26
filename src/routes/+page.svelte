3ca2d2e64d566ab486a9fb2782a7a34ae1494037
Albab Ghozari <albab9883@gmail.com>
2025-08-26 10:17:37 +0700

    commit awal

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+
+  
+
+</script>
+
+
+<div class="bg-amber-400 h-screen justify-center flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+  <div>
+    <!-- svelte-ignore a11y_img_redundant_alt -->
+    <img class="size-47 shadow-xl rounded-md" src="/cover.png" alt="Cover Image hero" />
+  </div>
+  <div class="bg-blue-600 flex flex-col items-center md:items-start">
+    <span class="text-2xl font-medium">Class Warfare</span>
+    <span class="font-medium text-sky-500">The Anti-Patterns</span>
+    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+    <a href="/login">login</a>
+  </div>
+</div>
+
